68929cd221d386f2ceb3b781822de7fc6acd4160
Gökhan Demirci <gokhandemirciofficial@gmail.com>
2026-01-06 22:38:29 +0300

    Restaurant sınıfı ve menü listesi oluşturulmuştu. Son değişiklikler yapıldı.

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -2,30 +2,48 @@ package model;
 
 import java.util.ArrayList;
 
+/**
+ * Sistemdeki bir restoranı temsil eden sınıf.
+ * <p>
+ * Bir restoranın adını, puanını ve sahip olduğu menü öğelerini (Yemek/İçecek)
+ * tutar. Menüye ürün ekleme ve listeleme işlemlerini yönetir.
+ * </p>
+ * * @author Gökhan Demirci
+ */
+
 public class Restaurant {
     private String name;
-    private double rating;
-    private ArrayList<MenuItem> menu; // Yemek listesi
+    private double rating; // YENİ: Puan değişkeni eklendi
+    private ArrayList<MenuItem> menu;
 
+    // Constructor hem isim hem puan alıyor
     public Restaurant(String name, double rating) {
         this.name = name;
         this.rating = rating;
         this.menu = new ArrayList<>();
     }
 
-    // Menüye yemek ekleme
+    // Main.java'da kullanılan puanı getiren metod
+    public double getRating() {
+        return rating;
+    }
+
     public void addMenuItem(MenuItem item) {
         menu.add(item);
     }
 
-    // Menüyü listeleme
     public void listMenu() {
         System.out.println("--- " + name + " Menüsü ---");
         for (MenuItem item : menu) {
-            System.out.println(item);
+            System.out.println(item); // MenuItem'ın toString() metodu varsa güzel yazar
         }
     }
 
-    public String getName() { return name; }
-    public ArrayList<MenuItem> getMenu() { return menu; }
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<MenuItem> getMenu() {
+        return menu;
+    }
 }
